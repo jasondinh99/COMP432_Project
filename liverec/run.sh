649671772c778ed09a39ecbf8f817b2fa35a94b0
Jason Dinh <49790297+jasondinh99@users.noreply.github.com>
2023-04-23 10:47:50 -0400

    Revert "LiveRec Model"
    This reverts commit d6f3abf19c3b2a3535105778bcf0943ff38db604.

--- liverec/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python main.py --model=LiveRec \
-               --dataset="dataset/" \
-               --fr_ctx \
-               --fr_rep \
-               --model_to="liverec" \
-               --device="cuda" \
-               --caching 
